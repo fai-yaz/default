8bab220c2328d2b37bf17d872f47acb4a7e705a7
fai-yaz <fai.dev@pm.me>
2023-11-27 00:06:32 +0000

    new custom directory

---
+++ custom/configs/vimtex.lua
@@ -0,0 +1,11 @@
+-- custom/configs/vimtex.lua
+
+-- VimTeX configuration for custom PDF viewer
+-- vim.g.vimtex_view_general_viewer = 'open'
+-- vim.g.vimtex_view_general_options = '-a Google Chrome %p'
+
+
+
+-- removes latexmk from opening pdf normally
+-- vim.g.vimtex_view_general_options_latexmk = '-view=none'
+
